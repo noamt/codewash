edc841536136982b36ff4e44c52aa4264cca56d7
noamt <noam@10ne.org>
2015-11-08 10:27:17 -0800

    Add a background layer and two dirt tiles

--- web/js/game.js
+++ web/js/game.js
@@ -1,13 +1,27 @@
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
         var game = new PixelJS.Engine();
+
         game.init({
             container: 'game_container',
             width: 800,
-            height: 500
+            height: 450
         });
 
-        game.run(function (elapsedTime, dt) {
+        var backgroundLayer = game.createLayer('background');
+        var grass = backgroundLayer.createEntity();
+        backgroundLayer.static = true;
+        grass.pos = { x: 0, y: 0 };
+        grass.asset = new PixelJS.Tile();
+        grass.asset.prepare({
+            name: 'dirt2.png',
+            size: {
+                width: 800,
+                height: 450
+            }
+        });
+
+        game.loadAndRun(function (elapsedTime, dt) {
         });
     }
 };
